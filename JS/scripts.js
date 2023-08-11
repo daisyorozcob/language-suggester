2dacc4bbd3b48421283c32aefc613ddc01eec7e5
daisyorozcob <daisyyy.ob@gmail.com>
2023-08-11 11:20:02 -0700

    add if and else if statements

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,3 +4,19 @@ const suggestionPara = document.getElementById('language-suggestion');
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
+
+  const q1Value = form.q1.value;
+
+  let languageSuggestion = '';
+
+  if (q1Value === 'web') {
+    languageSuggestion = 'JavaScript';
+  } else if (q1Value === 'mobile') {
+    languageSuggestion = 'Swift';
+  } else if (q1Value === 'system') {
+    languageSuggestion = 'C#';
+  }
+
+  suggestionPara.textContent = languageSuggestion;
+  resultDiv.classList.remove('hidden');
+});
