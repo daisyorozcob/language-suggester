ef79cfe83e93a064160fb187ee6cfe3c45025fea
daisyorozcob <daisyyy.ob@gmail.com>
2023-08-11 13:17:01 -0700

    add new question add to scripts

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,17 +6,24 @@ form.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const q1Value = form.q1.value;
+  const q2Value = form.q2.value.toLowerCase();
 
   let languageSuggestion = '';
 
   if (q1Value === 'web') {
-    languageSuggestion = 'JavaScript';
-  } else if (q1Value === 'mobile') {
+    if (q2Value.includes('dog')) {
+      languageSuggestion = 'JavaScript';
+    } else {
+      languageSuggestion = 'Python';
+    }
+  } else if (q1Value === 'app') {
     languageSuggestion = 'Swift';
-  } else if (q1Value === 'system') {
+  } else if (q1Value === 'sys') {
     languageSuggestion = 'C#';
   }
 
   suggestionPara.textContent = languageSuggestion;
   resultDiv.classList.remove('hidden');
+  form.reset();
 });
+
